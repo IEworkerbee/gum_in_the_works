9f6614b1beda7023e9db4a6b6d12dcc7eaeac382
KaeganKoski <kizaegan@gmail.com>
2026-08-03 19:11:56 -0800

    Added some base scenes for developing Machine editor logic. Also added a shader which smooths non integer scaled pixel art. Needs to be added to every pixel sprite. Only added to machine stuff so far.

--- gum_in_the_works/scripts/entities/motherGum.gd
+++ gum_in_the_works/scripts/entities/motherGum.gd
@@ -8,7 +8,7 @@ extends StaticBody2D
 @onready var perimeter_coords: Array[Vector2i] = [tm_position]
 @onready var all_coords: Array[Vector2i] = []
 
-var scene_weeping_sugarbell = preload("res://scenes/entities/plants/weeping_sugarbells.tscn")
+var scene_weeping_sugarbell = preload("res://scenes/entities/plants/weepingSugarbells.tscn")
 var priority: int = 2
 var planted: Array[bool] = []
 var processing_sig: bool = true
@@ -35,23 +35,23 @@ func expand() -> int:
 	var surrounding_non_cells_placeable = surrounding_cells.filter(tile_map.filter_texture_cells_placeable.bind(distance_from_center).bind(tm_position).bind(Vector2i(1,0)).bind(1))
 	var coord_to_pick = surrounding_non_cells_placeable.pick_random()
 	tile_map.place_tile_smart(coord_to_pick, 2)
-	
+
 	all_coords.append(coord_to_pick)
 	planted.append(false)
-	
+
 	if tile_map.get_surrounding_cells(coord_to_pick).filter(tile_map.filter_texture_cells_placeable.bind(distance_from_center).bind(tm_position).bind(Vector2i(1,0)).bind(1)).size() > 0:
 		# If any neighbors are placeable tiles, add this coordinate to perimeter_coords
 		perimeter_coords.append(coord_to_pick)
 	for coord in tile_map.get_surrounding_cells(coord_to_pick).filter(tile_map.filter_non_texture_cells.bind(2)):
-		# For coords in neighbors with texture 
+		# For coords in neighbors with texture
 		if tile_map.get_surrounding_cells(coord).filter(tile_map.filter_texture_cells_placeable.bind(distance_from_center).bind(tm_position).bind(Vector2i(1,0)).bind(1)).size() == 0:
 			# If coord has no placeable neighbors, remove it from perimeter
 			perimeter_coords.erase(coord)
 	if surrounding_non_cells_placeable.size() == 1:
-		# If no neighbors of original pick are placeble tiles, remove choice from perimeter 
+		# If no neighbors of original pick are placeble tiles, remove choice from perimeter
 		perimeter_coords.erase(choice)
 	return 1
-	
+
 func _plant() -> int:
 	processing_sig = false
 	randomize()
@@ -71,5 +71,3 @@ func _plant() -> int:
 	planted[choice_idx] = true
 	processing_sig = true
 	return 1
-	 
-	
--- gum_in_the_works/scripts/scene_map/tempScene.gd
+++ gum_in_the_works/scripts/scene_map/tempScene.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
 
-var scene_ribose_crystal = preload("res://scenes/entities/ribose_crystal.tscn")
-var scene_mother_gum = preload("res://scenes/entities/mother_gum.tscn")
+var scene_ribose_crystal = preload("res://scenes/entities/riboseCrystal.tscn")
+var scene_mother_gum = preload("res://scenes/entities/motherGum.tscn")
 var ribose_crystal_instance
 var mother_gum_instance
 var pos: Vector2i = Vector2i(-3, -2)
@@ -13,7 +13,7 @@ func _ready():
 	mother_gum_instance.global_position = tile_map_layer.map_to_local(Vector2i(0, -1))
 	mother_gum_instance.global_position.y += 8
 	mother_gum_instance.y_sort_enabled = true
-	add_child(mother_gum_instance)    
+	add_child(mother_gum_instance)
 
 func _process(_delta: float):
 	if is_instance_valid(ribose_crystal_instance) == false:
@@ -22,7 +22,3 @@ func _process(_delta: float):
 		ribose_crystal_instance.global_position.y += 16
 		ribose_crystal_instance.y_sort_enabled = true
 		add_child(ribose_crystal_instance)
-		
-		
-	
-	
