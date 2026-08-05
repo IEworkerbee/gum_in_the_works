ea9309be247b31f50e3fcdbf515d7e010fca2f93
KaeganKoski <kizaegan@gmail.com>
2026-08-04 17:59:32 -0800

    added removal of the bolt when pressing b on the same machine component. Also made it so you cannot add bolts when the machine case simulation is running.

--- gum_in_the_works/scripts/machines/machineComponents/class/MachineComponent.gd
+++ gum_in_the_works/scripts/machines/machineComponents/class/MachineComponent.gd
@@ -49,19 +49,27 @@ func _input(event: InputEvent) -> void:
 			gravity_scale = 1.0 if is_playing else 0.0
 
 		# Allows an machine component to be boltable
-		elif event.keycode == KEY_B and draggable:
-			var bolt_position: Vector2 = get_bolt_point()
-			var pin_joint: PinJoint2D = PinJoint2D.new()
-			pin_joint.global_position = bolt_position
-			pin_joint.node_a = get_parent().get_node("Static-backboard").get_path()
-			pin_joint.node_b = self.get_path()
-			get_parent().add_child(pin_joint)
+		elif event.keycode == KEY_B and draggable and !is_playing:
+			if !has_node("BoltSprite2D"):
+				var bolt_position: Vector2 = get_bolt_point()
+				var pin_joint: PinJoint2D = PinJoint2D.new()
+				pin_joint.name = "BoltPinJoint2D"
+				pin_joint.global_position = bolt_position
+				pin_joint.node_a = get_parent().get_node("Static-backboard").get_path()
+				pin_joint.node_b = self.get_path()
+				get_parent().add_child(pin_joint)
 
-			var bolt_sprite: Sprite2D = Sprite2D.new()
-			add_child(bolt_sprite)
-			bolt_sprite.texture = load("res://assets/images/machineComponents/bolt.png")
-			bolt_sprite.global_position = bolt_position
-			
+				var bolt_sprite: Sprite2D = Sprite2D.new()
+				bolt_sprite.name = "BoltSprite2D"
+				add_child(bolt_sprite)
+				bolt_sprite.texture = load("res://assets/images/machineComponents/bolt.png")
+				bolt_sprite.global_position = bolt_position
+			else:
+				$BoltSprite2D.queue_free()
+				await $BoltSprite2D.tree_exited
+				get_parent().get_node("BoltPinJoint2D").queue_free()
+				await get_parent().get_node("BoltPinJoint2D").tree_exited
+
 
 func save_init_state():
 	init_state.transform = global_transform
